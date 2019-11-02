c6e95c8453ba982baccd16bfef77c0befd480c82
scottbing <sbing52@comcast.net>
2019-11-01 23:44:25 -0600

    SB_Widgets
    SB_Widgets

--- SB_Widgets/app/src/main/java/com/here/android/example/sb_widgets/MainActivity.java
+++ SB_Widgets/app/src/main/java/com/here/android/example/sb_widgets/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,12 +22,13 @@ public class MainActivity extends AppCompatActivity
     Spinner spinner;
     Switch contentSwitch;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contentView = (ViewGroup) ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);        Spinner spinner = new Spinner(this);
+        contentView = (ViewGroup) ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);        
 
         spinner = new Spinner(this);
         contentView.addView(spinner);
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity
     private void setupSwitch() {
         contentSwitch = (Switch)findViewById(R.id.switch1);
         contentSwitch.setChecked(true);
-        changeSource("colors");
+//        changeSource("colors");
         contentSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
@@ -68,20 +70,79 @@ public class MainActivity extends AppCompatActivity
                     R.array.planets_array, android.R.layout.simple_spinner_item);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(this);
                 break;
         }
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
-        String color = (String)parent.getItemAtPosition(i);
-        switch(color) {
-            case "Red":
-                contentView.setBackgroundColor(Color.RED);
-                break;
-            default:
-                break;
-
+        if (contentSwitch.isChecked()) {
+            Log.d("switch","colors");
+            String color = (String)parent.getItemAtPosition(i);
+            switch(color) {
+                case "Red":
+                    contentView.setBackgroundColor(Color.RED);
+                    break;
+                case "Blue":
+                    contentView.setBackgroundColor(Color.BLUE);
+                    break;
+                case "White":
+                    contentView.setBackgroundColor(Color.WHITE);
+                    break;
+                case "Yellow":
+                    contentView.setBackgroundColor(Color.YELLOW);
+                    break;
+                case "Black":
+                    contentView.setBackgroundColor(Color.BLACK);
+                    break;
+                case "Green":
+                    contentView.setBackgroundColor(Color.GREEN);
+                    break;
+                case "Purple":
+                    contentView.setBackgroundColor(Color.parseColor("#800080"));
+                    break;
+                case "Orange":
+                    contentView.setBackgroundColor(Color.parseColor("#ffa500"));
+                    break;
+                case "Grey":
+                    contentView.setBackgroundColor(Color.parseColor("#808080"));
+                    break;
+                default:
+                    contentView.setBackgroundColor(Color.WHITE);
+                    break;
+            }
+        } else {
+            Log.d("switch", "planets");
+            String planet = (String) parent.getItemAtPosition(i);
+            switch (planet) {
+                case "Mercury":
+                    Log.d("Planets", "Mercury");
+                    break;
+                case "Venus":
+                    Log.d("Planets", "Venus");
+                    break;
+                case "Earth":
+                    Log.d("Planets", "Earth");
+                    break;
+                case "Mars":
+                    Log.d("Planets", "Mars");
+                    break;
+                case "Jupiter":
+                    Log.d("Planets", "Jupiter");
+                    break;
+                case "Saturn":
+                    Log.d("Planets", "Saturn");
+                    break;
+                case "Uranus":
+                    Log.d("Planets", "Uranus");
+                    break;
+                case "Neptune":
+                    Log.d("Planets", "Neptune");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
